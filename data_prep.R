48743d68bb5f58c6f30aaf485c6ab4ebbec4d12f
Maina <maina@SCI-9390.local>
2021-10-01 18:44:04 +1000

    #extacted sea floor proportion for each planning unit

--- data_prep.R
+++ data_prep.R
@@ -2,7 +2,6 @@ _________________
 ##download Gurobi Optimizer fom the website https://www.gurobi.com/downloads/gurobi-software/
 ##obtain the academic license
 ##install the license on the terminal using: grbgetkey ad0445fe-fcf2-11eb-ba3c-0242ac120002
-<<<<<<< HEAD
 ##grbgetkey fa525344-ff4c-11eb-b77b-0242ac120002# license server
 #info  : GRB_LICENSE_FILE=/Users/maina/Gurobi/gurobi.lic
 =======
@@ -42,7 +41,8 @@ rm(list=ls())
 #read eez file
 wio.eez<-readOGR(dsn='~/Documents/tbca/Cleaned/WIO_EEZ/','WIO_EEZ')
 wio.shelf<-readOGR(dsn='~/Documents/tbca/Cleaned/WIO_Geomorphic/','Shelf')
-wio.slope<-readOGR(dsn='~/Documents/tbca/Cleaned/WIO_Geomorphic/','Slope')
+#wio.slope<-readOGR(dsn='~/Documents/tbca/Cleaned/WIO_Geomorphic/','Slope')
+#WIOinland<-readOGR(dsn='~/Documents/tbca/PlanningUnits/WIO_PUfile_base/','WIO_EEZ_Inland_fin')
 
 
 ##select countries of interest from eez and delete columns
@@ -59,12 +59,13 @@ afr.alb<-CRS("+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 +x_0=0 +y_0=0 +d
 
 roi.wio.alb<- spTransform(wio.eez, afr.alb)
 
+
 #create planning units of 1km2
 source("makegrid.R")
 # hex - without clipping
+#hex_grid_shelf <- make_grid(wio.shelf, type = "hexagonal", cell_area = 1000000, clip = FALSE)
 hex_grid_shelf <- make_grid(wio.shelf, type = "hexagonal", cell_area = 1000000, clip = FALSE)
-hex_grid_slope <- make_grid(wio.slope, type = "hexagonal", cell_area = 1000000, clip = FALSE)
-hex_grid_eez <- make_grid(roi.wio.alb, type = "hexagonal", cell_area = 25000000, clip = FALSE)
+hex_grid_eez <- make_grid(roi.wio.alb, type = "hexagonal", cell_area = 25000000, clip = TRUE)
 
 #plot(roi.wio.alb, col = "grey50", bg = "light blue", axes = FALSE)
 #plot(hex_grid, border = "orange", add = TRUE)
@@ -76,27 +77,27 @@ hex_grid_eez <- make_grid(roi.wio.alb, type = "hexagonal", cell_area = 25000000,
 #box()
 
 # Extract polygon/p ID's
-pid.shelf <- sapply(slot(hex_grid_shelf, "polygons"), function(x) slot(x, "ID")) 
-pid.slope <- sapply(slot(hex_grid_slope, "polygons"), function(x) slot(x, "ID")) 
+#pid.shelf <- sapply(slot(hex_grid_shelf, "polygons"), function(x) slot(x, "ID")) 
+pid.small <- sapply(slot(hex_grid_small, "polygons"), function(x) slot(x, "ID")) 
 pid.eez <- sapply(slot(hex_grid_eez, "polygons"), function(x) slot(x, "ID"))
 
 # Create dataframe with correct rownames
-p.df.shelf <- data.frame( ID=1:length(hex_grid_shelf), row.names = pid.shelf)    
-p.df.slope <- data.frame( ID=1:length(hex_grid_slope), row.names = pid.slope)  
+#p.df.shelf <- data.frame( ID=1:length(hex_grid_shelf), row.names = pid.shelf)    
+p.df.small <- data.frame( ID=1:length(hex_grid_small), row.names = pid.small)  
 p.df.eez <- data.frame( ID=1:length(hex_grid_eez), row.names = pid.eez)
 
 # Try coersion again and check class
-pu.shelf <- SpatialPolygonsDataFrame(hex_grid_shelf, p.df.shelf)
-class(pu.shelf) 
-pu.slope <- SpatialPolygonsDataFrame(hex_grid_slope, p.df.slope)
-class(pu.slope) 
+#pu.shelf <- SpatialPolygonsDataFrame(hex_grid_shelf, p.df.shelf)
+#class(pu.shelf) 
+pu.small <- SpatialPolygonsDataFrame(hex_grid_small, p.df.small)
+class(pu.small) 
 pu.eez <- SpatialPolygonsDataFrame(hex_grid_eez, p.df.eez)
 class(pu.eez) 
 
 ##please write Large data to (~/volumes/Data)
-writeOGR(obj=pu.shelf,dsn='~/Documents/tbca/PlanningUnits', layer="wioShelf_1km", driver="ESRI Shapefile",overwrite_layer=TRUE)
-writeOGR(obj=pu.slope,dsn='~/Documents/tbca/PlanningUnits', layer="wioSlope_1km", driver="ESRI Shapefile",overwrite_layer=TRUE)
-writeOGR(obj=pu.eez,dsn='~/Documents/tbca/PlanningUnits', layer="wioEEZ_1km", driver="ESRI Shapefile",overwrite_layer=TRUE)
+#writeOGR(obj=pu.shelf,dsn='~/Documents/tbca/PlanningUnits', layer="wioShelf_1km", driver="ESRI Shapefile",overwrite_layer=TRUE)
+writeOGR(obj=pu.small,dsn='~/Documents/tbca/PlanningUnits', layer="wioSmall_1km", driver="ESRI Shapefile",overwrite_layer=TRUE)
+writeOGR(obj=pu.eez,dsn='~/Documents/tbca/PlanningUnits', layer="wioEEZ_5km", driver="ESRI Shapefile",overwrite_layer=TRUE)
 
 rm(list=ls())
 
@@ -119,6 +120,51 @@ hex_cover <- gIntersection(hex_grid_c, MYPOLYGONS, byid = TRUE) %>%
 hex_cover$cover_area[is.na(hex_cover$cover_area)] <- 0
 hex_cover$pct_cover <- 100 * hex_cover$cover_area /hex_cover$area
 
+
+
+
+##tbca dataprep
+#Sept 14
+##readin the shapefile
+setwd("~/Documents/tbca/Cleaned/WIO_Geomorphic/")
+tbca.pu<-readOGR(dsn='~/Documents/tbca/Cleaned/Planning_units/','TBCA_500m_units')
+files<-list.files( pattern='shp$')
+
+#plot(tbca.pu)
+
+tbca.pu@data$PU<-rownames(tbca.pu@data)#add PU ID
+
+#delete shelf classification
+#files<-files[-14]
+#files<-files[-3]
+#files<-files[-4]
+hc<-list()
+pi<-list()
+hc<-list()
+sh<-list()
+outpout<- list()
+for (i in seq_along(files)){
+  tryCatch({
+  sh[[i]]<-readOGR(dsn='.',tools::file_path_sans_ext(files[i]))
+  hc[[i]]<-raster::crop(sh[[i]], extent(tbca.pu))
+  pi[[i]] <- raster::intersect( hc[[i]], tbca.pu)
+  pi[[i]]$area <- area(pi[[i]]) / 1000000
+  outpout[[i]]<-aggregate(area~PU+ Geomorphic, data=pi[[i]]@data, FUN=sum)
+  #write.csv(outpout[[i]],paste('/Users/josephmaina/OneDrive - Macquarie University/Projects/,files[i],'.csv'))		
+  }, error=function(e){})
+} 
+
+puvspr<-do.call(rbind,outpout)
+
+colnames(puvspr)<-c('pu','species','amount')
+r.seafloor = rle(puvspr$species)
+puvspr$species.id <- rep(seq_along(r.seafloor$lengths), r.seafloor$lengths)
+puvspr<-puvspr[order(puvspr$pu),]
+
+puvspr.seasfloor.id<-puvspr[,c('species.id','pu','amount','species')]
+#colnames(puvspr.seasfloor.id)[1]<-'species'
+
+write.csv(puvspr.seasfloor.id,'~/Documents/tbca/wiompan_mpa/puvspr.seafloor.csv')
 
 ##############
 #Priortize R
