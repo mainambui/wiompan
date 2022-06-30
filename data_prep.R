545899218ca230b6f4664455bca8c0875128f033
Maina <maina@SCI-9390.local>
2022-06-30 13:23:03 +1000

    dataProcessing

--- data_prep.R
+++ data_prep.R
@@ -248,6 +248,51 @@ write.csv(puvspr.id,'~/Documents/tbca/wiompan_mpa/tbca.planning/puvspr.seagrass.
 rm(list=ls())
 
 
+########mangrove presence asence
+#read in plannimng units
+tbca.pu<-readOGR(dsn='~/Documents/tbca/PlanningUnits/TBCA_PU_file_8Jun2022/','tbca_pu_file_country_empty')
+tbca.pu@data$PU<-rownames(tbca.pu@data)#add PU ID
+
+#plot(tbca.pu)
+#files<-list.files(path='~/Dropbox/WIOMPAN/Data/WIOMangrove_MainaEtAl/Mangrove_Data/R356_mdp_mangrove_v3_vector/', pattern='shp$')
+#dat<-readOGR(dsn="~/Dropbox/WIOMPAN/Data/WIOMangrove_MainaEtAl/Mangrove_Data/R356_mdp_mangrove_v3_vector/",tools::file_path_sans_ext(files[1]))#change to 1 or 2 for coral and seagrass rspectively
+
+files<-list.files(path='~/Documents/tbca/Data/Cleaned/WIO_Mangroves_WCMC/', pattern='shp$')
+dat<-readOGR(dsn="~/Documents/tbca/Data/Cleaned/WIO_Mangroves_WCMC/",tools::file_path_sans_ext(files[1]))#change to 1 or 2 for coral and seagrass rspectively
+
+
+dat1<-raster::crop(dat, extent(tbca.pu))
+#wio_eez_seagrass
+
+#convert to sf due to large memory##https://stackoverflow.com/questions/45128670/combining-spatialpointsdataframe-with-spatialpolygonsdataframe-error-maximum-re
+dat.sf<-st_as_sf(dat1)
+tbca.sf<-st_as_sf(tbca.pu)
+
+#check th lengths of levels omn category2 field in coral data. N per levcel seems sufficient. now use to breakup the data
+#dat1@data %>% 
+#  group_by(CATEGORY2) %>%
+#  summarise(no_rows = length(CATEGORY2))
+
+##run intersection of the cropped extent
+#dat2 <- dat.sf%>%
+#  group_by(CATEGORY2) %>%
+#  do(sf::st_intersection(., tbca.sf))
+
+dat2 <- st_intersection(dat.sf, tbca.sf)
+
+dat3<-st_as_sf(dat2)
+dat3$area <- sf::st_area(dat3)/1000000
+dat3$species<-"mangrove"
+puvspr<-aggregate(area~PU + species, data=dat3, FUN=sum)
+colnames(puvspr)<-c('pu','species','amount')
+dat.ga = rle(puvspr$species)
+puvspr$species.id <- rep(seq_along(dat.ga$lengths), dat.ga$lengths)
+puvspr<-puvspr[order(puvspr$pu),]
+puvspr.id<-puvspr[,c('species.id','pu','amount','species')]
+write.csv(puvspr.id,'~/Documents/tbca/wiompan_mpa/tbca.planning/puvspr.mangrove.allen.csv')
+rm(list=ls())
+
+
 #pending layers
 #gravity of markkets
 #climate change
@@ -266,10 +311,10 @@ options(tibble.width = Inf)
 library(data.table)
 setwd("/Users/maina/Documents/tbca/wiompan_mpa/tbca.planning/")
 temp = list.files(path="/Users/maina/Documents/tbca/wiompan_mpa/tbca.planning/",pattern="*.csv")
-sppList = lapply(temp[2:4], read.csv)
+sppList = lapply(temp, read.csv)
 puvspr_dat<-do.call(rbind,sppList)
 puvspr_dat$species<-as.factor(puvspr_dat$species)
-spec_dat<-data.frame(as.vector(levels(puvspr_dat$species)), seq(1:15),0.3,1)
+spec_dat<-data.frame(as.vector(levels(puvspr_dat$species)), seq(1:18),0.3,1)
 spec_dat<-spec_dat[,c(2,3,4,1)]
 colnames(spec_dat)<-c("id","prop","spf","name")
 puvspr_dat <-puvspr_dat %>% mutate(species = species %>% as.factor() %>% as.numeric())
